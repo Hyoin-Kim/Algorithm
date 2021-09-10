206964d821fac492bb16ba7554b84005bb5fe9d3
Hyoin-kim <hi980506@naver.com>
2021-09-10 21:58:46 +0900

    [완료] 예산

---
+++ Programmers/Lv1/solution08.js
@@ -0,0 +1,13 @@
+//예산
+function solution(d, budget) {
+    var answer = 0;
+    let count = 0;
+    let array = d.sort((a,b)=>a-b);
+    for(let i =0;i<d.length;i++){
+        answer +=d[i];
+        if(answer <= budget){
+            count++;
+        }
+    }
+    return count;
+}
